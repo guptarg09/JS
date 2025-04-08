e7212f4e930ea86154a66681a95d91586dfb58fa
guptarg09 <guptarg3a@gmail.com>
2025-04-08 16:39:45 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12223
+let accountEmail = "ritesh@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 4321  // not allowed
+accountEmail = "gupta@gmail.com"
+accountPassword = "12345"
+accountCity = "Mumbai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
