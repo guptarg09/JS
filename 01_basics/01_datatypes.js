f593cb17b293faffc7d1be16da7ae26e6dff7aab
guptarg09 <guptarg3a@gmail.com>
2025-04-08 16:40:16 +0000

    datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";   // treat all js code as newer version
+
+// alert(3 + 3)  // we are using nodejs not browser
+
+let name = "Ritesh"
+let age = 20
+let isLoggedin = true
+
+console.log(typeof "name")
+console.log(typeof age)
+console.log(typeof isLoggedin)
+
+console.log(typeof undefined)
+console.log(typeof null)
