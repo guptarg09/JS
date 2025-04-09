c58c9ff91c94e7e232a5c296001d0a1c092c2db6
guptarg09 <guptarg3a@gmail.com>
2025-04-09 20:24:35 +0000

    object litrals done

---
+++ 02_basics/2.3_objects.js
@@ -0,0 +1,37 @@
+
+// *************object litrals************* 
+
+const mySymbol = Symbol("key1")
+
+const jsUser = {
+    name : "Ritesh",
+    "full name" : "Ritesh Gupta",
+    age : 18,
+    location : "Jaipur",
+    isLoggedin : false,
+    lastLoginDays : ["Monday", "Saturday"],
+    [mySymbol] : "mykey1"   // using symbol in objects
+}
+
+console.log(jsUser.name)  // you can't get full name in this manner 
+console.log(jsUser["age"])  // prefered approach
+
+console.log(jsUser["full name"])
+
+console.log(jsUser[mySymbol])
+
+jsUser.age = 21   // changing value
+// Object.freeze(jsUser)
+
+// ----function-----
+
+jsUser.greeting = function(){
+    console.log("hello js user")
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`hello js user, ${this.name}`)
+}
+
+console.log(jsUser.greeting())
+console.log(jsUser.greetingTwo())
