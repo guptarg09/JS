3364a29e5cf5025d1773a9c7eee8a998ea758847
guptarg09 <guptarg3a@gmail.com>
2025-04-09 17:00:51 +0000

    nums and math done

---
+++ 01_basics/1.5_nums_and_math.js
@@ -0,0 +1,32 @@
+// ****************nums*****************
+
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const newNumber = 37.4431
+// console.log(newNumber.toPrecision(3))
+
+// const anotherNumber = 35367.4431
+// console.log(anotherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// ****************maths*****************
+
+console.log(Math.abs(-4))
+console.log(Math.min(3, 5, 1, 5))
+console.log(Math.max(3, 5, 1, 5))
+
+console.log(Math.random())
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
