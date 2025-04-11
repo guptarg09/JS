3b987c188d8476694001ef1552d2c7a7929fa863
guptarg09 <guptarg3a@gmail.com>
2025-04-11 16:12:19 +0530

    Update 2.2_arrays.js

--- 02_basics/2.2_arrays.js
+++ 02_basics/2.2_arrays.js
@@ -13,10 +13,10 @@ console.log(mergedArr);
 const all_elements = [...arr1, ...arr2];
 console.log(all_elements);
 
-console.log(Array.isArrsy("Ritesh"));
-console.log(Array.from("Ritesh"));
+console.log(Array.isArray("Ritesh")); //false  
+console.log(Array.from("Ritesh"));   //[ 'R', 'i', 't', 'e', 's', 'h' ]
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
-console.log(Array.of(score1, score2, score3))
+console.log(Array.of(score1, score2, score3))  //[ 100, 200, 300 ]
